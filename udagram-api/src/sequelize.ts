130a87b35e5660a5755fa8d9f08078cdf254c5b5
lhazy111 <lhazy111@googlemail.com>
2021-05-16 15:10:12 +0100

    backend working ver

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,5 +1,5 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 
 export const sequelize = new Sequelize({
