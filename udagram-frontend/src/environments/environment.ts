ea6ca2ea0b924ff869a9e1dfd62a0f68e7dc51e2
lhazy111 <lhazy111@googlemail.com>
2021-05-24 22:04:50 +0100

    resubmission

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,10 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  //apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4620a178bff64f9b94f17c877135f8c-183267707.eu-west-2.elb.amazonaws.com:8080/api/v0'
+  //abbea3372f48247a7aa781e21178fa0b-1936198396.eu-west-2.elb.amazonaws.com
+  //apiHost: 'http://aa81b626eb9854f7eb592097db7d95b6-818601733.echoeu-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
@@ -15,4 +18,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
